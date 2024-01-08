8736511d2d19bbc95fa17180ab10d3641dd62918
Dcoream <direy23@gmail.com>
2024-01-08 08:42:01 -0600

    Se incluye GuardarMarcador

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { Schema } = mongoose; // Agregamos la importación de Schema
+const { Schema } = mongoose;
 const app = express();
 const PORT = 3000;
 
@@ -10,11 +10,19 @@ mongoose.connect('mongodb://localhost:27017/Maptrack', { useNewUrlParser: true, 
 
 const coordenadaSchema = new mongoose.Schema({
   coordenadas: {
-    type: Schema.Types.Mixed,  // Cambiamos el tipo de dato a Mixed
+    type: Schema.Types.Mixed,
   },
 }, { versionKey: false });
 
 const Coordenada = mongoose.model('Coordenada', coordenadaSchema);
+
+const marcadorSchema = new mongoose.Schema({
+  marcadores: {
+    type: Schema.Types.Mixed,
+  },
+}, { versionKey: false });
+
+const Marcador = mongoose.model('Marcador', marcadorSchema);
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -27,13 +35,30 @@ app.post('/guardarPoligono', async (req, res) => {
 
     const resultadoInsercion = await Coordenada.create({ coordenadas });
 
-    // No incluimos el campo __v en la respuesta
     const { _id } = resultadoInsercion.toObject();
     console.log('Resultado de la inserción:', { _id });
 
     res.json({ mensaje: 'Coordenadas guardadas correctamente' });
   } catch (error) {
     console.error('Error al guardar coordenadas:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+app.post('/guardarMarcadores', async (req, res) => {
+  const { marcadores } = req.body;
+
+  try {
+    console.log('Marcadores recibidos en el backend:', marcadores);
+
+    const resultadoInsercion = await Marcador.create({ marcadores });
+
+    const { _id } = resultadoInsercion.toObject();
+    console.log('Resultado de la inserción:', { _id });
+
+    res.json({ mensaje: 'Marcadores guardados correctamente' });
+  } catch (error) {
+    console.error('Error al guardar marcadores:', error);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
